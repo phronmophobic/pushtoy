73036182bdc31b1699354b5ff2611358c5f89a7a
Adrian Smith <smith.adriane@gmail.com>
2013-09-10 17:24:46 -0700

    removing some dead code, comments, extraneous loggings.

--- project.clj
+++ project.clj
@@ -6,29 +6,6 @@
   :dependencies [
                  [com.palletops/runit-crate "0.8.0-alpha.1"]
                  [com.palletops/java-crate "0.8.0-beta.5"]
-
                  [com.palletops/pallet "0.8.0-RC.1"]
-
-                 ;; [ch.qos.logback/logback-classic "1.0.9"]
                  [com.palletops/app-deploy-crate "0.8.0-alpha.3"]]
-
-;;   :dependencies [
-;;                  [com.palletops/runit-crate "0.8.0-alpha.1"]
-;;                  [com.palletops/java-crate "0.8.0-beta.4"]
-;;                  ;; [org.slf4j/slf4j-api "1.6.1"]
-;;                  [com.palletops/pallet "0.8.0-RC.1"]
-;;                  ;; [ch.qos.logback/logback-core "1.0.0"]
-;;                  ;; [ch.qos.logback/logback-classic "1.0.0"]
-;;                  [ch.qos.logback/logback-classic "1.0.9"]
-;;                  [com.palletops/app-deploy-crate "0.8.0-alpha.1"]
-
-;;                  ;; [clj-logging-config "1.9.10"]
-;;                  ;; [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-;;                  ;;                                    javax.jms/jms
-;;                  ;;                                    com.sun.jdmk/jmxtools
-;;                  ;;                                    com.sun.jmx/jmxri]]
-;; ]
-  ;; :resource-paths ["resources"]
-
-  ;; :exclusions [commons-logging]
   :eval-in-leiningen true)
